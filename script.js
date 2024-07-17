2e9270ebe860d7301989f2d880686ad3d6f043d8
Dev Dheer <ddheer157@gmail.com>
2024-07-17 16:31:20 +0530

    third

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
+(function () {
+    const locomotiveScroll = new LocomotiveScroll();
+})();
 
 function menutl() {
 const navmenutl = gsap.timeline()
@@ -58,13 +57,12 @@ close.addEventListener('click', () => {
 }
 menutl()
 
-
 function homepage(){
 const homepagetl = gsap.timeline()
 
 homepagetl.from('.bg-home video', {
-    width: '200vw',
-    height: '200vh',
+    width: '300vw',
+    height: '300vh',
     duration: 1.5,
     ease: "expo.inOut",
 })
@@ -91,3 +89,129 @@ homepagetl.from('.right-home',{
 })
 }
 homepage()
+
+function madeinmadras(){
+gsap.to('.video-sec-1 ', {
+    scrollTrigger: {
+      trigger: '.video-sec-1',
+      start: 'top center',
+      end: 'bottom center',
+      scrub: true,
+    },
+    scale: 1.5 
+  });
+}
+madeinmadras()
+
+
+function cursorfollower(){
+    let cursorfollower = document.querySelector('.cursor-follower')
+    let cursoreffect1 = document.querySelectorAll('.left-home h1, .right-home svg')
+    let cursoreffect2 = document.querySelectorAll('a')
+
+    window.addEventListener('mousemove', (e) => {
+        gsap.to(cursorfollower, {
+            x: e.clientX + 10,
+            y: e.clientY + 10,
+            duration: .5,
+            display: 'block',
+            rotation: e.movementX * 0.9,
+        })
+    })
+
+    // cursoreffect1
+
+    cursoreffect1.forEach(cursor1 => {
+        cursor1.addEventListener("mouseenter", () => {
+            gsap.to(cursorfollower, {
+                width: "4vw",
+                height: "4vw",
+                rotate: '360deg',
+                borderRadius: '1vw',
+                duration: 0.2,
+            });
+        });
+    });
+
+    cursoreffect1.forEach(cursor1 => {
+        cursor1.addEventListener("mouseleave", () => {
+            gsap.to(cursorfollower, {
+                width: "2vw",
+                height: "2vw",
+                rotate: '-360deg',
+                borderRadius: '0',
+                duration: 0.2,
+            });
+        });
+    });
+
+    // cursoreffect2
+
+    cursoreffect2.forEach(cursor2 => {
+        cursor2.addEventListener("mouseenter", () => {
+            gsap.to(cursorfollower, {
+                rotate: '360deg',
+                duration: .2,
+            });
+        });
+    });
+
+    cursoreffect2.forEach(cursor2 => {
+        cursor2.addEventListener("mouseleave", () => {
+            gsap.to(cursorfollower, {
+                rotate: '-360deg',
+                duration: 0.2,
+            });
+        });
+    });
+    
+}
+cursorfollower()
+
+function marqueetl(){
+    window.addEventListener('wheel', (dets) =>{
+        if(dets.deltaY > 0){
+            gsap.to('.marquee', {
+                transform: ('translateX(-200%)'),
+                repeat: -1,
+                duration: 4,
+                ease: 'none',
+            })
+            gsap.to('.marquee svg', {
+                rotate: '0deg',
+            })
+        }
+        else{
+            gsap.to('.marquee', {
+                transform: ('translateX(100%)'),
+                repeat: -1,
+                duration: 4,
+                ease: 'none',
+            })
+            gsap.to('.marquee svg', {
+                rotate: '360deg',
+            })
+        }
+    })
+}
+marqueetl()
+
+function motorcyclesanimation() {
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
+    })
+    elemC.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+motorcyclesanimation()
