c114d2295b91862a435fda70f4ffe71f9df5f82f
Dev Dheer <ddheer157@gmail.com>
2024-07-17 10:52:56 +0530

    second

--- script.js
+++ script.js
@@ -9,10 +9,16 @@ const navmenutl = gsap.timeline()
 navmenutl.to('.menu', {
     display: 'block',
     width: '100vw',
-    height: "100vh",
+    height: "10vh",
     top: '50%',
     bottom: 0,
-    borderRadius: '0%',
+    duration: .3,    
+})
+
+navmenutl.to('.menu', {
+    display: 'block',
+    width: '100vw',
+    height: "100vh",
     duration: .5,    
 })
 
@@ -25,18 +31,16 @@ navmenutl.from('.header-menu h1, .menu hr', {
 navmenutl.from('.links-menu a', {
     y: 30,
     opacity: 0,
-    duration: 1.5,
+    duration: .5,
     stagger: 0.05,
-    ease: 'elastic.out(1,0.3)'
 })
 
 navmenutl.from('.social-menu a, .social-menu h1', {
     y: 30,
     opacity: 0,
-    duration: 1.5,
+    duration: .5,
     stagger: 0.05,
-    ease: 'elastic.out(1,0.3)',
-    delay: -1,
+    delay: -.5
 })
 
 navmenutl.pause()
@@ -54,13 +58,15 @@ close.addEventListener('click', () => {
 }
 menutl()
 
+
+function homepage(){
 const homepagetl = gsap.timeline()
 
 homepagetl.from('.bg-home video', {
     width: '200vw',
     height: '200vh',
     duration: 1.5,
-    ease: 'elastic.out(1,0.3)',
+    ease: "expo.inOut",
 })
 
 homepagetl.from('.navbar', {
@@ -73,12 +79,15 @@ homepagetl.from('.left-home',{
     x: -100,
     opacity: 0,
     duration: 1.5,
-    ease: 'elastic.out(1,0.1)',
+    ease: 'elastic.out(1,0.4)',
 })
 
 homepagetl.from('.right-home',{
     x: 100,
     opacity: 0,
     duration: 1,
-    ease: 'elastic.out(1,0.5)',
+    ease: 'elastic.out(1,0.4)',
+    delay: -.9
 })
+}
+homepage()
