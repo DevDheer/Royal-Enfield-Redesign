0dca0849b89fd47015f8d6a7500aa7e3a5322ca9
Dev Dheer <ddheer157@gmail.com>
2024-07-18 21:07:44 +0530

    sixth

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
     const locomotiveScroll = new LocomotiveScroll();
 })();
 
-function navbar(){
-    window.addEventListener('wheel', (dets) =>{
-        if(dets.deltaY > 0){
+function navbar() {
+    window.addEventListener('wheel', (dets) => {
+        if (dets.deltaY > 0) {
             gsap.to('.navbar', {
                 transform: ('translateY(-100%)'),
                 duration: .4,
             })
         }
-        else{
+        else {
             gsap.to('.navbar', {
                 transform: ('translateY(0%)'),
                 duration: .4,
@@ -21,108 +21,148 @@ function navbar(){
 navbar()
 
 function menutl() {
-const navmenutl = gsap.timeline()
+    const navmenutl = gsap.timeline()
 
-navmenutl.to('.menu', {
-    display: 'block',
-    width: '100vw',
-    height: "10vh",
-    top: '50%',
-    bottom: 0,
-    duration: .3,    
-})
+    navmenutl.to('.menu', {
+        display: 'block',
+        width: '20vw',
+        height: "20vh",
+        top: '50%',
+        bottom: 0,
+        duration: .3,
+    })
 
-navmenutl.to('.menu', {
-    display: 'block',
-    width: '100vw',
-    height: "100vh",
-    duration: .5,    
-})
+    navmenutl.to('.menu', {
+        display: 'block',
+        width: '100vw',
+        height: "100vh",
+        duration: 1,
+        ease: 'elastic.out(1,0.4)',
+    })
 
-navmenutl.from('.header-menu h1, .menu hr', {
-    y: 30,
-    opacity: 0,
-    stagger: 0.05,
-})
+    navmenutl.from('.header-menu h1, .menu hr', {
+        y: 30,
+        opacity: 0,
+        stagger: 0.05,
+    })
 
-navmenutl.from('.links-menu a', {
-    y: 30,
-    opacity: 0,
-    duration: .5,
-    stagger: 0.05,
-})
+    navmenutl.from('.links-menu a', {
+        y: 30,
+        opacity: 0,
+        duration: .5,
+        stagger: 0.05,
+    })
 
-navmenutl.from('.social-menu a, .social-menu h1', {
-    y: 30,
-    opacity: 0,
-    duration: .5,
-    stagger: 0.05,
-    delay: -.5
-})
+    navmenutl.from('.social-menu a, .social-menu h1', {
+        y: 30,
+        opacity: 0,
+        duration: .5,
+        stagger: 0.05,
+        delay: -.5
+    })
 
-navmenutl.pause()
+    navmenutl.from('.video-menu', {
+        width: 0,
+        height: 0,
+        opacity: 0,
+        duration: .5,
+        ease: 'elastic.out(1,0.4)',
+    })
 
-const open = document.querySelector('.navigate-1')
-const close = document.querySelector('.navigate-2')
+    navmenutl.pause()
 
-open.addEventListener('click', () => {
-    navmenutl.play();
-})
+    const open = document.querySelector('.navigate-1')
+    const close = document.querySelector('.navigate-2')
 
-close.addEventListener('click', () => {
-    navmenutl.reverse();
-})
+    open.addEventListener('click', () => {
+        navmenutl.play();
+    })
+
+    close.addEventListener('click', () => {
+        navmenutl.reverse();
+    })
 }
 menutl()
 
-function homepage(){
-const homepagetl = gsap.timeline()
+function menutl2() {
+    const menuvideo = document.querySelector('#video-gt-menu')
+    let menutl2 = gsap.timeline()
+    menutl2.to('.overlay-menu', {
+        top: 0,
+        display: 'flex',
+        borderRadius: '1vw',
+        duration: .5,
+        delay: 0,
+        ease: 'elastic.out(1,0.4)',
+    })
 
-homepagetl.from('.bg-home video', {
-    width: '300vw',
-    height: '300vh',
-    duration: 1.5,
-    ease: "expo.inOut",
-})
+    menutl2.from('.overlay-menu', {
+        y: 30,
+        opacity: 0,
+        duration: .5,
+    })
+    menutl2.pause()
+    menuvideo.addEventListener('mouseenter', () => {
+        menutl2.play()
+    })
+    menuvideo.addEventListener('mouseleave', () => {
+        menutl2.reversed()
+    })
+}
+menutl2()
 
-homepagetl.from('.navbar', {
-    y: -100,
-    opacity: 0,
-    stagger: 0.05,
-})
+function homepage() {
+    const homepagetl = gsap.timeline()
 
-homepagetl.from('.left-home',{
-    x: -100,
-    opacity: 0,
-    duration: 1.5,
-    ease: 'elastic.out(1,0.4)',
-})
+    homepagetl.from('.bg-home video', {
+        width: '300vw',
+        height: '300vh',
+        duration: 1.5,
+        ease: "expo.inOut",
+    })
 
-homepagetl.from('.right-home',{
-    x: 100,
-    opacity: 0,
-    duration: 1,
-    ease: 'elastic.out(1,0.4)',
-    delay: -.9
-})
+    homepagetl.from('.navbar', {
+        y: -100,
+        opacity: 0,
+        stagger: 0.05,
+    })
+
+    homepagetl.from('.left-home h1 span', {
+        y: 100,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.05,
+        ease: 'elastic.out(1,0.3)',
+    })
+
+    homepagetl.from('.right-home', {
+        x: 100,
+        opacity: 0,
+        duration: 2,
+        ease: 'elastic.out(10,0.5)',
+        delay: -.9
+    })
 }
 homepage()
 
-function madeinmadras(){
-gsap.to('.video-sec-2 ', {
-    scrollTrigger: {
-      trigger: '.video-sec-2',
-      start: 'top center',
-      end: 'bottom center',
-      scrub: true,
-    },
-    scale: 1.3 
-  });
+function madeinmadras() {
+
+    gsap.to('.video-sec-2 ', {
+        scrollTrigger: {
+            trigger: '.video-sec-2',
+            start: 'top center',
+            end: 'bottom center',
+            scrub: true,
+        },
+        scale: 1.5,
+    });
+
+
 }
 madeinmadras()
 
 
-function cursorfollower(){
+function cursorfollower() {
     let cursorfollower = document.querySelector('.cursor-follower')
     let cursoreffect1 = document.querySelectorAll('.left-home h1, .right-home svg')
     let cursoreffect2 = document.querySelectorAll('a')
@@ -182,13 +222,13 @@ function cursorfollower(){
             });
         });
     });
-    
+
 }
 cursorfollower()
 
-function marqueetl(){
-    window.addEventListener('wheel', (dets) =>{
-        if(dets.deltaY > 0){
+function marqueetl() {
+    window.addEventListener('wheel', (dets) => {
+        if (dets.deltaY > 0) {
             gsap.to('.marquee', {
                 transform: ('translateX(-200%)'),
                 repeat: -1,
@@ -199,7 +239,7 @@ function marqueetl(){
                 rotate: '0deg',
             })
         }
-        else{
+        else {
             gsap.to('.marquee', {
                 transform: ('translateX(100%)'),
                 repeat: -1,
@@ -229,31 +269,31 @@ function motorcyclesanimation() {
         e.addEventListener("mouseenter", function () {
             var image = e.getAttribute("data-image")
             fixed.style.backgroundImage = `url(${image})`;
-            })
         })
+    })
 
 }
 
-function accessories(){
+function accessories() {
     let next = document.querySelector('#next')
     let previous = document.querySelector('#previous')
 
-        next.addEventListener('click', () => {
-            gsap.to('.accessory', {
-                transform: 'translateX(-250%)',
-            })
-        })    
+    next.addEventListener('click', () => {
+        gsap.to('.accessory', {
+            transform: 'translateX(-250%)',
+        })
+    })
 
-        previous.addEventListener('click', () => {
-            gsap.to('.accessory', {
-                transform: 'translateX(0%)',
-            })
-        })  
+    previous.addEventListener('click', () => {
+        gsap.to('.accessory', {
+            transform: 'translateX(0%)',
+        })
+    })
 
 }
 accessories()
 
-function gallery(){
+function gallery() {
     gsap.to('.images img', {
         transform: 'translateX(-600%)',
         duration: 40,
