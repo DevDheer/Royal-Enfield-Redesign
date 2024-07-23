185b87df2d3f0ee879327580344d20e01adc3a27
Dev Dheer <ddheer157@gmail.com>
2024-07-23 17:26:40 +0530

    final

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ function loader() {
         onComplete: homepage
     })
 }
-// loader()
+loader()
 
 function homepage() {
     const homepagetl = gsap.timeline()
@@ -80,7 +80,7 @@ function homepage() {
         delay: -.9
     })
 }
-// homepage()
+homepage()
 
 function navbar() {
     window.addEventListener('wheel', (dets) => {
